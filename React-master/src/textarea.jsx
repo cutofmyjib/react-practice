e7931aa8ed89f4cf408e1e353926767bc6149415
cutofmyjib <thedianalozano@gmail.com>
2015-11-16 14:52:41 -0800

    create TextArea component

---
+++ React-master/src/textarea.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+
+module.exports = React.createClass({
+  render: function() {
+    return(
+      <div className="text-area">
+        <div className="text-header">
+          <ul>
+            <li><b>HOUR</b></li>
+            <li>DATE</li>
+          </ul>
+        </div>
+        <div className="text-body">
+          <h1 className="title">{this.props.title}</h1>
+          <p>{this.props.description}</p>
+        </div>
+        <div className="text-footer">
+          <ul>
+            <li id="modify"><a href="#">MODIFY</a></li>
+            <li><div className="bullet"></div>WORK CALENDAR</li>
+            <li><div className="bullet"></div>REMINDER 12:00</li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+});
