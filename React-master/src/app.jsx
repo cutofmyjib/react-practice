df3b66cc3cdbd8f99dcaad92157827dc40e2b001
cutofmyjib <thedianalozano@gmail.com>
2015-11-16 14:53:14 -0800

    replaced code with TextArea component

--- React-master/src/app.jsx
+++ React-master/src/app.jsx
@@ -1,29 +1,12 @@
 var React = require('react');
 var options = require('./object.json');
+var TextArea = require('./textarea')
 
 var Hello = React.createClass({
   render: function() {
     return(
       <footer>
-        <div className="text-area">
-          <div className="text-header">
-            <ul>
-              <li><b>HOUR</b></li>
-              <li>DATE</li>
-            </ul>
-          </div>
-          <div className="text-body">
-            <h1 className="title">{this.props.title}</h1>
-            <p>{this.props.description}</p>
-          </div>
-          <div className="text-footer">
-            <ul>
-              <li id="modify"><a href="#">MODIFY</a></li>
-              <li><div className="bullet"></div>WORK CALENDAR</li>
-              <li><div className="bullet"></div>REMINDER 12:00</li>
-            </ul>
-          </div>
-        </div>
+        <TextArea />
         <div className="users-container">
           <div className="users">
             <ul>
