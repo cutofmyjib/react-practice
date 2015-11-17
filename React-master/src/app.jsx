b1a403fa5d084a119c9856ef9b236afe09c6cda3
cutofmyjib <thedianalozano@gmail.com>
2015-11-16 17:29:59 -0800

    replace map div with Map component

--- React-master/src/app.jsx
+++ React-master/src/app.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var TextArea = require('./textarea');
 var Users = require('./users');
+var Map = require('./map');
 var options = require('./object.json');
 
 var Hello = React.createClass({
@@ -10,19 +11,7 @@ var Hello = React.createClass({
         <TextArea title={this.props.title}
         description={this.props.description}/>
         <Users />
-        <div className="map">
-          <div className="map-memo">
-            <p>Memo here</p>
-          </div>
-          <div className="map-img">
-            <img src="https://maps.googleapis.com/maps/api/staticmap?center=South+Beach,San+Francisco,CA&zoom=13&size=240x240&maptype=roadmap
-&markers=color:red%7Clabel:podcasthouse%7C37.786355,-122.391199
-&key=AIzaSyAJzQKtYZxDKxdZ16O_jYJCznIvMiyx_1k" />
-          </div>
-          <div className="map-footer">
-            <p>Address here</p>
-          </div>
-        </div>
+        <Map />
       </footer>
     )
   }
