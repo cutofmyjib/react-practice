65cfff903c4102314880792409b6199b2adba11b
cutofmyjib <thedianalozano@gmail.com>
2015-11-16 19:32:30 -0800

    take out chunk of code to create UsersList component

--- React-master/src/users.jsx
+++ React-master/src/users.jsx
@@ -3,14 +3,7 @@ var React = require('react');
 module.exports = React.createClass({
   render: function() {
     return (
-      <div className="users-container">
-        <div className="users">
-          <ul>
-            <li><div className="user-icon purple"><div className="smile"></div></div>{this.props.users}</li>
-            <li><div className="user-icon red"><div className="smile"></div></div>{this.props.users}</li>
-          </ul>
-        </div>
-      </div>
+      <li><div className={"user-icon " + this.props.color}><div className="smile"></div></div>{this.props.name}</li>
     )
   }
 })
