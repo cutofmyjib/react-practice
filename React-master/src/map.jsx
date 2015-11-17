33fb65cab3c2a756479519a91cca00c0885aa3b7
cutofmyjib <thedianalozano@gmail.com>
2015-11-16 17:31:54 -0800

    place map div in Map component

--- React-master/src/map.jsx
+++ React-master/src/map.jsx
@@ -4,7 +4,17 @@ module.exports = React.createClass({
   render: function() {
     return (
       <div className="map">
-        <p>I am a map</p>
+        <div className="map-memo">
+          <p>Memo here</p>
+        </div>
+        <div className="map-img">
+          <img src="https://maps.googleapis.com/maps/api/staticmap?center=South+Beach,San+Francisco,CA&zoom=13&size=240x240&maptype=roadmap
+&markers=color:red%7Clabel:podcasthouse%7C37.786355,-122.391199
+&key=AIzaSyAJzQKtYZxDKxdZ16O_jYJCznIvMiyx_1k" />
+        </div>
+        <div className="map-footer">
+          <p>Address here</p>
+        </div>
       </div>
     )
   }
