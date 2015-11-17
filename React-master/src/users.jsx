9580943c75aa194ff02decbf51b18fbb1eb14cf0
cutofmyjib <thedianalozano@gmail.com>
2015-11-16 17:27:18 -0800

    take user-container and save as Users component

--- React-master/src/users.jsx
+++ React-master/src/users.jsx
@@ -3,8 +3,13 @@ var React = require('react');
 module.exports = React.createClass({
   render: function() {
     return (
-      <div className="badge">
-        <p>MODIFY</p>
+      <div className="users-container">
+        <div className="users">
+          <ul>
+            <li><div className="user-icon purple"><div className="smile"></div></div>{this.props.users}</li>
+            <li><div className="user-icon red"><div className="smile"></div></div>{this.props.users}</li>
+          </ul>
+        </div>
       </div>
     )
   }
